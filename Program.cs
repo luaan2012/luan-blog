d44d7b99780766e333247a4318ae529113412cf1
luaan2012 <oempreg.yes@outlook.com.br>
2024-02-10 18:19:05 -0300

    ajuste msg

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Hello World! Esta dando certo meu cd/ci");
 
 app.Run();
