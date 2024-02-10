d05d60dad67426465d2cde5463e19e525f674742
luaan2012 <oempreg.yes@outlook.com.br>
2024-02-10 19:33:41 -0300

    Alterado as config do app

--- Program.cs
+++ Program.cs
@@ -1,6 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World! Esta dando certo meu cd/ci");
+var secrets = new Secret();
+
+app.Configuration.GetSection("Secrets").Bind(secrets);
+var connectionString = app.Configuration.GetConnectionString("DefaultConnection");
+
+app.MapGet("/", () => new {
+    ConnectionString = connectionString,
+    Secrets = secrets
+});
 
 app.Run();
---
+++ Secrets.cs
@@ -0,0 +1,5 @@
+public class Secret {
+    public string? JwtTokenSecret { get; set; }
+    public string? ApiKey { get; set; }
+    public string? PrivateKey { get; set; }
+}
