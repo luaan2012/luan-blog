89c5da43edb1778c593d52faa81e82c64ce30c6f
luaan2012 <oempreg.yes@outlook.com.br>
2024-02-10 20:45:14 -0300

    ajustes

--- Program.cs
+++ Program.cs
@@ -1,5 +1,13 @@
+using System.Text.RegularExpressions;
+using Azure.Storage.Blobs;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+var azureStorageConnectionString = 
+    app.Configuration.GetValue<string>("AzureStorageConnectionString");
+
+
 
 var secrets = new Secret();
 
@@ -11,4 +19,30 @@ app.MapGet("/", () => new {
     Secrets = secrets
 });
 
+app.MapPost("/", (Upload model) => {
+    UploadBase64Image(model.Image, "user-images");
+    return Results.Ok();
+});
+
+app.UseStaticFiles();
+
 app.Run();
+
+
+string UploadBase64Image(string base64Image, string container)
+{
+    var fileName = Guid.NewGuid().ToString() + ".jpg";
+    var data = new Regex(@"^data:image\/[a-z]+;base64,").Replace(base64Image, "");
+
+    byte[] imageBytes = Convert.FromBase64String(data);
+    var blobClient = new BlobClient(azureStorageConnectionString, container, fileName);
+
+    using (var stream = new MemoryStream(imageBytes))
+    {
+        blobClient.Upload(stream);
+    }
+
+    return blobClient.Uri.AbsoluteUri;
+}
+
+public record Upload(string Image);
